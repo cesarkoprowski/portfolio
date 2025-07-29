e5bcc6ba90a2e82fa47b0076af032e79791ae626
Cesar Koprowski <173203133+cesarkoprowski@users.noreply.github.com>
2025-07-29 00:53:16 -0300

    feat: implement navbar and main page

---
+++ src/components/Navbar.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { Home, FolderGit, PanelLeft } from "lucide-vue-next";
+import Button from "@/components/ui/button/Button.vue";
+import Label from "@/components/ui/label/Label.vue";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+// Menu items.
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Projects",
+    url: "#",
+    icon: FolderGit,
+  },
+];
+</script>
+
+<template>
+  <Sidebar>
+    <SidebarContent class="bg-[#0a0c10]">
+      <SidebarGroup>
+        <SidebarGroupLabel class="text-white flex justify-between">
+          <Label class="text-lg">Cesk.</Label>
+          <Button variant="ghost" class="hover:text-white w-6 h-8">
+            <PanelLeft class="h-5 w-5" />
+          </Button>
+        </SidebarGroupLabel>
+        <SidebarGroupContent class="pt-2">
+          <SidebarMenu>
+            <SidebarMenuItem v-for="item in items" :key="item.title">
+              <SidebarMenuButton
+                asChild
+                class="hover:bg-[#101319] active:bg-[#101319] transition-colors duration-200"
+              >
+                <a :href="item.url">
+                  <component :is="item.icon" />
+                  <span>{{ item.title }}</span>
+                </a>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroupContent>
+      </SidebarGroup>
+    </SidebarContent>
+  </Sidebar>
+</template>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Navbar from "@/components/Navbar.vue";
+import { SidebarProvider } from "@/components/ui/sidebar";
+</script>
+
+<template>
+  <SidebarProvider>
+    <Navbar />
+    <main>
+      <RouterView />
+    </main>
+  </SidebarProvider>
+</template>
